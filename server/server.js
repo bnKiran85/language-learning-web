9db7df89996bb9f084c826e9a47f728a135709f9
bnKiran85 <bnkiran85@gmail.com>
2026-04-03 20:38:27 +0530

    fixed backend url

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
